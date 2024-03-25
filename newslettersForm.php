76aa35afda6082c2e003964d06965e7388c9edc8
Theo Alleaume <theo.alleaume024@gmail.com>
2024-03-25 23:25:14 +0100

    Etape 3 : Incomplète

---
+++ newslettersForm.php
@@ -0,0 +1,27 @@
+<?php
+    $db = json_decode(file_get_contents('db.json'), true);
+    $db = $db['newsletters'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire Newsletters</title>
+</head>
+<body>
+
+    <h1>Sélectionnez une newsletter à envoyer :</h1>
+    <form method="POST" action="">
+        <select name="newsletter_id">
+            <?php foreach ($db as $newsletter){ ?>
+                <option value="<?= $newsletter['id'] ?>"><?= $newsletter['title'] ?></option>
+            <?php} ?>
+        </select>
+        <input type="submit" value="Envoyer Newsletter">
+    </form>
+    
+</body>
+</html>
