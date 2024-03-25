1f3a6e3ba25ca9c26c42b9bdc92ddf380c321d90
Theo Alleaume <theo.alleaume024@gmail.com>
2024-03-25 22:50:51 +0100

    Etape 2

--- newsletters.php
+++ newsletters.php
@@ -1,17 +1,58 @@
-<?php
-    $newsletters = json_decode(file_get_contents('http://51.91.108.32/newsletters'), true);
 
-    if (!empty($newsletters)) {
-        foreach ($newsletters as $newsletter) {
-            echo "<h2>" . $newsletter['titre'] . "</h2>";
-            echo "<table border='1'>";
-            echo "<tr><th>Email</th><th>Nom</th></tr>";
-            foreach ($newsletter['inscrits'] as $inscrit) {
-                echo "<tr><td>" . $inscrit['email'] . "</td><td>" . $inscrit['nom'] . "</td></tr>";
-            }
-            echo "</table>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Identifiants</th>
+                <th>Titre</th>
+                <th>Contenu</th>
+                <th>Interests</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+                $newsletters = json_decode(file_get_contents('http://51.91.108.32/newsletters'), true);
+                foreach ($newsletters as $newsletter):
+            ?>
+                <tr>
+                    <td><?= $newsletter['id']?></td>
+                    <td><?= $newsletter['title']?? "Aucun Titre"?></td>
+                    <td><?= $newsletter['content']?? "Aucun Contenu" ?></td>
+                    <td>
+                     <?php if(isset($newsletter['interests'])): ?>
+                        <?php foreach($newsletter['interests'] as $interest): ?>
+                            <ul>
+                                <?="<li>".$interest ."</li>"?>
+                            </ul>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        <?= "Aucun Interet" ?>
+                    <?php endif; ?>
+                    </td>
+                    
+                    <td><?= $newsletter['status']?? "En Attente" ?></td>
+                </tr>
+            <?php endforeach;?>
+        </tbody>
+
+    </table>
+    <?php
+
+        
+        if (!empty($newsletters)) {
+            
+        } else {
+            echo "Aucune newsletter trouvée.";
         }
-    } else {
-        echo "Aucune newsletter trouvée.";
-    }
-?>
+    ?> 
+</body>
+</html>
